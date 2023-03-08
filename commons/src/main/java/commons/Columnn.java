283c237a075d337e4b1cef1b42bd11d3c1f75f21
Wiktor Cupiał <w.m.cupial@student.tudelft.nl>
2023-03-08 13:41:52 +0100

    started working on boardController, added simple checkstyle and fixed minor problems with new cs rules

--- commons/src/main/java/commons/Columnn.java
+++ commons/src/main/java/commons/Columnn.java
@@ -1,10 +1,26 @@
 package commons;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Columnn {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    public Columnn(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return "Columnn{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- server/src/main/java/server/api/BoardController.java
+++ server/src/main/java/server/api/BoardController.java
@@ -2,12 +2,17 @@ package server.api;
 
 import java.util.List;
 import commons.Board;
+import commons.Columnn;
 import server.database.BoardRepository;
 
+import org.springframework.data.util.Pair;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/api/boards")
@@ -16,10 +21,7 @@ public class BoardController {
     private final BoardRepository repo;
 
     public BoardController(BoardRepository repo) {
-        System.err.println("hello from controller");
         this.repo = repo;
-        Board test = new Board("test");
-        repo.save(test);
     }
     
     @GetMapping(path = {"", "/"})
@@ -31,4 +33,33 @@ public class BoardController {
         return res;
     }
 
+    // wip
+    // still crashes for some reason :(
+    // -------
+    // the idea is to pass id of a column and board and this function will be able
+    // to add this column to the board
+    @PostMapping(path = "/add")
+    public ResponseEntity<Board> addColumn(@RequestBody Pair<Long, Long> req) {
+        Long boardId = req.getFirst();
+        // Columnn columnn = req.getSecond();
+        Long columnId = req.getSecond();
+        Board board = null;
+        Columnn column = null;
+        System.err.println("reached milestone 1");
+        try {
+            board = repo.getById(boardId);
+            // column = columnRepo.getById(columnId);
+        }
+        catch(Exception e) {
+            System.err.println("getById failed: " + boardId);
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+        System.err.println("reached milestone 2");
+        System.err.println(board);
+        if(board == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(board);
+    }
 }
