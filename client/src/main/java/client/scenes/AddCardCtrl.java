1339b4fdc6c96c197e4388798e46d04ad8107adb
Bobi Marinov <b.k.marinov-1@student.tudelft.nl>
2023-03-21 19:39:50 +0100

    removed imports

--- client/src/main/java/client/scenes/AddCardCtrl.java
+++ client/src/main/java/client/scenes/AddCardCtrl.java
@@ -6,9 +6,13 @@ import commons.Card;
 
 import jakarta.ws.rs.WebApplicationException;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
