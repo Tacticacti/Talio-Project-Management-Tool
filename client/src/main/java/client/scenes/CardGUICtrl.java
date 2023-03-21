d2998fe273c0f20062c97c148980ec5d773ec890
Edward Oh <j.oh-2@student.tudelft.nl>
2023-03-21 14:13:16 +0100

    Drag and Drop feature

--- client/src/main/java/client/scenes/CardGUICtrl.java
+++ client/src/main/java/client/scenes/CardGUICtrl.java
@@ -2,10 +2,16 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.Card;
+import javafx.scene.input.*;
+import javafx.scene.layout.AnchorPane;
 
 public class CardGUICtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    public AnchorPane card;
+
+    private Dragboard board;
 
     @Inject
     public CardGUICtrl(ServerUtils server, MainCtrl mainCtrl) {
@@ -16,4 +22,19 @@ public class CardGUICtrl {
     public void details(){
         mainCtrl.showAddCard();
     }
+
+    public void dragDetected(MouseEvent event) {
+        board = card.startDragAndDrop(TransferMode.MOVE);
+        ClipboardContent content = new ClipboardContent();
+        content.putString(card.getId());
+        board.setContent(content);
+        event.consume();
+    }
+
+    public void onDrag(DragEvent event) {
+        System.out.println(card + " is being dragged!");
+
+    }
+
+
 }
