63a402cfedc2b5d66de8cbd3d84b225286a1adc8
Edward Oh <j.oh-2@student.tudelft.nl>
2023-03-23 19:53:39 +0100

    Optimized imports and fixed checkstyle issues

--- client/src/main/java/client/scenes/CardGUICtrl.java
+++ client/src/main/java/client/scenes/CardGUICtrl.java
@@ -2,25 +2,15 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 
 public class CardGUICtrl {
-    private final ServerUtils server;
     private final MainCtrl mainCtrl;
     public AnchorPane card;
-    ClipboardContent content;
-
-    private Dragboard board;
 
     @Inject
     public CardGUICtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
-        this.server = server;
     }
 
     public void details(){
