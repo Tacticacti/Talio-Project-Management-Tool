f4a5543936efce9721e2d587f8d9224e3adaefb6
Bobi Marinov <b.k.marinov-1@student.tudelft.nl>
2023-03-29 11:06:27 +0200

    dealt with null title upon card creation

--- client/src/main/java/client/scenes/SingleBoardCtrl.java
+++ client/src/main/java/client/scenes/SingleBoardCtrl.java
@@ -60,6 +60,7 @@ import javafx.util.Duration;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.UUID;
 
@@ -309,94 +310,6 @@ public class SingleBoardCtrl implements Initializable {
             throw new RuntimeException(e);
         }        
     }
-
-    /*
-    public void addCard(VBox parent){
-        BoardList boardList = (BoardList) parent.getUserData();
-        long listid = boardList.getId();
-        TextInputDialog titleinput = new TextInputDialog();
-        titleinput.setTitle("Task Title");
-        titleinput.setHeaderText("Create new task");
-        titleinput.setContentText("Enter task title:");
-        titleinput.showAndWait().ifPresent(title ->{
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cardGUI.fxml"));
-            CardGUICtrl controller = new CardGUICtrl(server, mainCtrl);
-            fxmlLoader.setController(controller);
-            Card newCard = new Card(title);
-            try {
-                Node card = (Node) fxmlLoader.load();
-                Label titleLabel = (Label) card.lookup("#taskTitle");
-                titleLabel.setText(title);
-                Button detailButton = (Button) card.lookup("#details");
-                detailButton.setOnAction(event -> enterCard(card, parent));
-                nodeCardMap.put(card, newCard);
-                int index =parent.getChildren().size()-1;
-                if(parent.getChildren().size()==1){
-                    index=0;
-                }
-                card.setOnDragDetected(event -> {
-                    board = card.startDragAndDrop(TransferMode.MOVE);
-                    content = new ClipboardContent();
-                    content.putString(card.getId());
-                    board.setContent(content);
-
-                    // Create a snapshot of the current card
-                    WritableImage snapshot = card.snapshot(new SnapshotParameters(), null);
-                    ImageView imageView = new ImageView(snapshot);
-                    imageView.setFitWidth(card.getBoundsInLocal().getWidth());
-                    imageView.setFitHeight(card.getBoundsInLocal().getHeight());
-
-                    // Set the custom drag view to only show the current card being dragged
-                    board.setDragView(imageView.getImage(), event.getX(), event.getY());
-                    System.out.println("On Drag Detected: " + parent);
-                    System.out.println(parent.getChildren());
-                    event.consume();
-                });
-                card.setOnDragOver(event -> {
-                    if (board.hasString()) {
-    //                    System.out.println("Card " + board.getString() + " is being dragged!");
-                        event.acceptTransferModes(TransferMode.MOVE);
-                    }
-                    event.consume();
-                });
-                card.setOnDragDone(event -> {
-                    if (board.hasString()) {
-                        var cardParent = (VBox) card.getParent();
-                        cardParent.getChildren().remove(card);
-                    }
-                    System.out.println("On Drag Done: " + parent);
-                    System.out.println(parent.getChildren());
-                    event.consume();
-                });
-                card.setOnDragDropped(event -> {
-                    System.out.println("On Drag Dropped: " + parent);
-                    boolean success = false;
-                    if (board.hasString()) {
-                        System.out.println("Hello");
-                        System.out.println(parent.getChildren());
-                        parent.getChildren().add(0, card);
-                        System.out.println("world");
-                        success = true;
-                        System.out.println(parent.getChildren());
-                    }
-                    event.setDropCompleted(success);
-                    event.consume();
-                });
-                parent.getChildren().add(index, card);
-                Card saved = server.addCard(newCard);
-                newCard.setId(saved.getId());
-                //server.addCardToList(1L, 0L, newCard);
-                // long listIndex = getListIndex(boardId, listid);
-                saveCardToList(BOARDID, listid, newCard);
-                refresh();
-                //enterCard(card);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-     */
-
     public void setCardDetail(Node cardNode, VBox parent){
 //        AnchorPane list = (AnchorPane) parent.getParent(); unused variable
         BoardList boardList = (BoardList) parent.getUserData();
@@ -484,9 +397,6 @@ public class SingleBoardCtrl implements Initializable {
             }
         }
         server.addCard(current);
-        // long listIndex = getListIndex(boardId, listid);
-        // saveCardToList(1l,0l,current);
-        // server.updateCardFromList(1L, listIndex, current);
         updateCardFromList(BOARDID, listId, current);
         
         Stage popup = (Stage) source.getScene().getWindow();
@@ -497,27 +407,32 @@ public class SingleBoardCtrl implements Initializable {
     public void addNewCard(VBox parent){
         BoardList boardList = (BoardList) parent.getUserData();
         long listId = boardList.getId();
+        Optional<String> cardTitle = showTitleDialog();
+        if (cardTitle.isPresent()) {
+            Card newCard = new Card(cardTitle.get());
+            placeCard(parent, newCard);
+            Card saved = server.addCard(newCard);
+            newCard.setId(saved.getId());
+            saveCardToList(BOARDID, listId, newCard);
+            refresh();
+        }
+
+    }
+    public Optional<String> showTitleDialog(){
         TextInputDialog titleInput = new TextInputDialog();
         titleInput.setTitle("Task Title");
         titleInput.setHeaderText("Create new task");
         titleInput.setContentText("Enter task title:");
-        titleInput.showAndWait().ifPresent(cardTitle ->{
+        Optional<String> result = titleInput.showAndWait();
+        if (result.isPresent() && result.get().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR,
+                    "Title cannot be empty!", ButtonType.OK);
+            alert.showAndWait();
 
-            //TODO: Fix case when cardTitle is empty
-            System.out.println("-----------------------------");
-            System.out.println(cardTitle);
-            System.out.println("-----------------------------");
+            return showTitleDialog();
+        }
+        return result;
 
-            Card newCard = new Card(cardTitle);
-            placeCard(parent, newCard);
-            Card saved = server.addCard(newCard);
-            newCard.setId(saved.getId());
-            //server.addCardToList(1L, 0L, newCard);
-           // long listIndex = getListIndex(BOARDID, listId);
-            saveCardToList(BOARDID, listId, newCard);
-            refresh();
-            //enterCard(card);
-        });
     }
 
     private void setDragAndDrop(VBox parent, Node cardNode) {
