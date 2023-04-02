43087b550d61211d3899246e1c2695c00b74fd32
Eduardo Hernandez Melian <e.hernandezmelian@student.tudelft.nl>
2023-04-02 13:31:50 +0200

    enter board after creating it by default and some code cleanup

--- client/src/main/java/client/scenes/BoardOverviewCtrl.java
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -70,27 +70,15 @@ public class BoardOverviewCtrl {
         // add board to current hbox
         if (last_row.getChildren().size() < MAX_BOARDS_IN_ROW) {
 
-            // create bord and style it
+            // load board and style it
             FXMLLoader loader = new FXMLLoader(getClass().getResource("AddedBoard.fxml"));
             AnchorPane board = loader.load();
-            //HBox.setMargin(board, new Insets(20, 20, 20, 20));
-
-            // enter clicked board
+            last_row.getChildren().add(board);
 
             Board new_board = server.addBoard(new Board());
 
-
-            // add created board
-            last_row.getChildren().add(board);
-
-            //Board new_board = new Board();
-            //System.out.println(new_board.getId());
-
-
-
-            //Board new_board = server.addBoard(new Board());
+            // enter board when you click it
             Button btn =  (Button) board.lookup("#enterButton");
-
             btn.setOnAction(event -> {
                 try {
                     enterBoard(new_board);
@@ -99,40 +87,28 @@ public class BoardOverviewCtrl {
                 }
             });
 
-            //.add(new_board);
-
-
-
+            // enter board after creating it by default
+            enterBoard(new_board);
         }
         // create a new row in vbox
         else {
 
-            // create bord and style it
+            // load board and style it
             FXMLLoader loader = new FXMLLoader(getClass().getResource("AddedBoard.fxml"));
             Node board = loader.load();
 
+            // add new row to vbox and style it to be consistent
             HBox hbox = new HBox();
             hbox.setSpacing(100);
-            //hbox.setPadding(new Insets(102, 0, 0, 102));
             HBox.setMargin(board, new Insets(0, 0, 0, 187.5));
 
             Board new_board = server.addBoard(new Board());
 
-
+            // add it to board overview
             hbox.getChildren().add(board);
-
-
-
-            // enter clicked board
-
-
-
-            // add created board
             board_rows.getChildren().add(hbox);
 
-
-           // Board new_board = server.addBoard(new Board());
-
+            // enter board when you click it
             Button btn =  (Button) board.lookup("#enterButton");
             btn.setOnAction(event -> {
                 try {
@@ -142,7 +118,8 @@ public class BoardOverviewCtrl {
                 }
             });
 
-
+            // enter board after creating it by default
+            enterBoard(new_board);
         }
 
     }
@@ -204,6 +181,9 @@ public class BoardOverviewCtrl {
                 addJoinedBoard(board);
                 localUtils.add(board.getId());
                 added_board = true;
+
+                enterBoard(board);
+                System.out.println("hello!");
             }
         }
 
