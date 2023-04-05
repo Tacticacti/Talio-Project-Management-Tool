54c237913ed427ff0ee5c1efefe513808efb459b
Bobi Marinov <b.k.marinov-1@student.tudelft.nl>
2023-04-05 11:17:40 +0200

    fixed subtasks same

--- client/src/main/java/client/scenes/CardCtrl.java
+++ client/src/main/java/client/scenes/CardCtrl.java
@@ -211,12 +211,14 @@ public class CardCtrl {
                 System.out.println("Text of cur subtask processed: " + cb.getText());
                 if (!current.getSubtasks().contains(cb.getText()))
                     current.addSubTask(cb.getText());
-                if (!current.getSubtasks().get(i).equals(cb.getText())
-                        && current.getSubtasks().contains(cb.getText())) {
-                    current.removeSubTask(cb.getText());
-                    current.addSubtaskAtIndex(cb.getText(), i);
-                    if (cb.isSelected()) {
-                        current.completeSubTask(cb.getText());
+                if(i<current.getSubtasks().size()){
+                    if (!current.getSubtasks().get(i).equals(cb.getText())
+                            && current.getSubtasks().contains(cb.getText())) {
+                        current.removeSubTask(cb.getText());
+                        current.addSubtaskAtIndex(cb.getText(), i);
+                        if (cb.isSelected()) {
+                            current.completeSubTask(cb.getText());
+                        }
                     }
                 }
             }
