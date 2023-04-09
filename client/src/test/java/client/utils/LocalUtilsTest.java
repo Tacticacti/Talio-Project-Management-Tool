21a12d06164bfa460855d82c444661356a89acc5
Edward Oh <j.oh-2@student.tudelft.nl>
2023-04-09 20:21:30 +0200

    Added new tests for LocalUtils class

--- client/src/test/java/client/utils/LocalUtilsTest.java
+++ client/src/test/java/client/utils/LocalUtilsTest.java
@@ -4,9 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LocalUtilsTest {
     private LocalUtils localUtils;
@@ -18,11 +29,13 @@ public class LocalUtilsTest {
 
     @Test
     public void testSetPath() throws IOException {
-        String path = "data/test";
+        String path = "       test";
         LocalUtils localUtils = new LocalUtils();
         localUtils.setPath(path);
+        File file = new File("data/test@saved_boards.txt");
+        assertTrue(file.exists());
 
-        Assertions.assertEquals(path, localUtils.getPath());
+        assertEquals(path, localUtils.getPath());
     }
 
     @Test
@@ -30,13 +43,23 @@ public class LocalUtilsTest {
         localUtils.setPath("data/test");
         localUtils.write();
         localUtils.add(123L);
-
         localUtils.fetch();
 
         Set<Long> expectedBoards = new TreeSet<>();
         expectedBoards.add(123L);
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
+    }
+
+    @Test
+    public void testFetchThrowIoException() throws IOException {
+        localUtils.setPath("data/test");
+        localUtils.write();
+        localUtils.add(123L);
+        localUtils.inited = false;
+        assertThrows(IOException.class, () -> {
+            localUtils.fetch();
+        });
     }
 
     @Test
@@ -47,7 +70,7 @@ public class LocalUtilsTest {
         Set<Long> expectedBoards = new TreeSet<>();
         expectedBoards.add(123L);
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
     }
 
     @Test
@@ -59,7 +82,7 @@ public class LocalUtilsTest {
         Set<Long> expectedBoards = new TreeSet<>();
         expectedBoards.add(123L);
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
     }
 
     @Test
@@ -70,7 +93,7 @@ public class LocalUtilsTest {
 
         Set<Long> expectedBoards = new TreeSet<>();
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
     }
 
     @Test
@@ -80,7 +103,7 @@ public class LocalUtilsTest {
 
         Set<Long> expectedBoards = new TreeSet<>();
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
     }
 
     @Test
@@ -88,7 +111,7 @@ public class LocalUtilsTest {
         localUtils.setPath("data/test");
         localUtils.add(123L);
 
-        Assertions.assertTrue(localUtils.isAdded(123L));
+        assertTrue(localUtils.isAdded(123L));
     }
 
     @Test
@@ -107,7 +130,7 @@ public class LocalUtilsTest {
         Set<Long> expectedBoards = new TreeSet<>();
         expectedBoards.add(123L);
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
     }
 
     @Test
@@ -118,6 +141,56 @@ public class LocalUtilsTest {
 
         Set<Long> expectedBoards = new TreeSet<>();
 
-        Assertions.assertEquals(expectedBoards, localUtils.getBoards());
+        assertEquals(expectedBoards, localUtils.getBoards());
+    }
+
+    @Test
+    public void testReadCustomization() {
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("data/customization.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("123:color:red");
+            printWriter.println("456:background:black");
+            printWriter.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        LocalUtils.readCustomization();
+        Map<Long, String> customizationData = CustomizationUtils.customizationData;
+        assertTrue(customizationData.containsKey(123L));
+        assertEquals(customizationData.get(123L), "color:red");
+        assertTrue(customizationData.containsKey(456L));
+        assertEquals(customizationData.get(456L), "background:black");
+        File file = new File("data/customization.txt");
+        file.delete();
+    }
+
+    @Test
+    public void testWriteCustomization() throws IOException {
+        // create a temporary file for testing
+        File file = new File("data/customization.txt");
+        String filePath = file.getAbsolutePath();
+        // set the customization data
+        Map<Long, String> customizationData = new HashMap<>();
+        customizationData.put(1L, "value1");
+        customizationData.put(2L, "value2");
+
+        // write the customization data to the file
+        CustomizationUtils.customizationData = customizationData;
+        LocalUtils.writeCustomization();
+        File directory = new File("data");
+        assertTrue(directory.exists());
+
+        // read the contents of the file
+        String contents = new String(Files.readAllBytes(Paths.get(filePath)));
+
+        // expected contents of the file
+        String expectedContents = "1:value1\r\n2:value2\r\n";
+
+        // compare the actual and expected contents of the file
+        assertEquals(expectedContents, contents);
+        file.delete();
     }
 }
